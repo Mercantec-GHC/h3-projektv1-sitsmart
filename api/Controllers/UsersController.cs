9acc6c6bfe7896867afac5f92e60f533682fd553
Jacobfis <jacobnf@live.dk>
2024-10-22 08:54:00 +0200

    Changed delete id to string

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -147,7 +147,7 @@ namespace API.Controllers
 
         // DELETE: api/Users/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(int id)
+        public async Task<IActionResult> DeleteUser(string id)
         {
             var user = await _context.Users.FindAsync(id);
             if (user == null)
